d73db52055fbcd544f7c0f1655005073e7f68ff1
Senthil <senthil.dhanase@gmail.com>
2024-02-20 21:12:46 +0530

    Added DataDriven Framework Project

--- IntroToMaven/src/main/java/myFirstMavenProj/LeafGround_TxtBox.java
+++ IntroToMaven/src/main/java/myFirstMavenProj/LeafGround_TxtBox.java
@@ -5,25 +5,33 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class OpenBrowser {
-
+public class LeafGround_TxtBox {
+	
 	WebDriver driver;
-	public void OpenGoogle() {
+	
+	public void Open_Brow() {
 		System.setProperty("webdriver.chrome.driver", "F:\\Senthil\\Testing\\Selenium_Setup\\Drivers\\chromedriver-win32\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.navigate().to("https://google.co.in");
-		WebElement TxtBox =driver.findElement(By.name("q"));
-		TxtBox.sendKeys("Chennai \n");
+		
+	}
+	
+	public void Handle_TxtBox() {
+		driver.navigate().to("https://www.leafground.com/input.xhtml");
+		WebElement TxtBox1 = driver.findElement(By.id("j_idt88:name"));
+		TxtBox1.clear();
+		TxtBox1.sendKeys("Hi There");
 	}
 
-	public void CloseBrow() {
-		driver.quit();
+	public void Close_Brow() {
+		driver.close();
 	}
 	public static void main(String[] args) {
-		OpenBrowser open = new OpenBrowser();
-		open.OpenGoogle();
-		open.CloseBrow();
+		
+		LeafGround_TxtBox act = new LeafGround_TxtBox();
+		act.Open_Brow();
+		act.Handle_TxtBox();
+		act.Close_Brow();
 
 	}
 
